59a2e871a0c625579bd908ab0fbff1f6b48914e5
Rad Szymula <radoszymula@gmail.com>
2024-02-24 09:44:21 -0600

    NOJIRA: make a better button core component

---
+++ src/components/core/RButton.tsx
@@ -0,0 +1,95 @@
+import { TouchableOpacity, Text, View, StyleSheet, Image } from "react-native";
+import { colors } from "../../context/themes";
+
+export function RButton({onPress, active = false, style = {}, children = null}){
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        styles.base,
+        active ? styles.active : styles.inactive,
+        style,
+      ]}
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+export function RButtonText({text, active = false, style = {}}){
+  return (
+    <Text
+      style={[
+        styles.textBase,
+        active ? styles.textActive : styles.textInactive,
+        style,
+      ]}
+    >
+      {text}
+    </Text>
+  )
+}
+
+export function RButtonImage({inactiveImage, activeImage = null, active = false, style = {}}){
+  return (
+    <View>
+      <Image
+        source={active ? activeImage : inactiveImage}
+        style={[styles.imageBase, style]}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  base: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    paddingLeft: 4,
+    paddingRight: 4,
+
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  active: {
+    backgroundColor: colors.link,
+    // color: colors.background,
+    borderColor: colors.link,
+  },
+  inactive: {
+    backgroundColor: colors.link,
+    // color: colors.background,
+    borderColor: colors.border,
+  },
+  textBase: {
+    fontSize: 12
+  },
+  textActive: {
+    color: colors.background,
+  },
+  textInactive: {
+    color: colors.background,
+  },
+  imageBase: {
+    width: 12,
+    height: 12,
+  },
+});
+
+
+
+// export function RButtonText({text, onPress, active = false, textStyle = {}, style = {}}){
+//   return (
+//     <RButton onPress={onPress} active={active} style={style}>
+//       <Text
+//         style={[
+//           styles.textBase,
+//           active ? styles.textActive : styles.textInactive,
+//           textStyle,
+//         ]}
+//       >
+//         {text}
+//       </Text>
+//     </RButton>
+//   )
+// }
