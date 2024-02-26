43adaed677ad5e6a0a3e6573ab32c24e7f962e0f
Rad Szymula <radoszymula@gmail.com>
2024-02-25 22:59:08 -0600

    NOJIRA: Fix timezone issue

--- src/screens/discuss/DiscussHome.tsx
+++ src/screens/discuss/DiscussHome.tsx
@@ -32,12 +32,24 @@ function getDateText(createdTimestamp){
   // console.log(moment.utc(new Date()))
 
   // const MS_IN_MIN = 60000
-  const res = moment(new Date(createdTimestamp)).local().fromNow()
+  console.log("DATEW",
+    createdTimestamp,
+    new Date(createdTimestamp),
+    moment(new Date(createdTimestamp)),
+    moment(new Date(createdTimestamp)).local(),
+    moment(new Date(createdTimestamp)).local,
+    moment.utc(new Date(createdTimestamp)),
+    moment.utc(createdTimestamp).local().format("YYYY-MM-DDTHH:mm:ss.SSSS")
+  )
+  // const res = moment(new Date(createdTimestamp)).local().fromNow()
   // const stamp = moment(new Date(createdTimestamp))
   // const now = moment(new Date());
   // const res = now.diff(stamp);
   // if(res)
   // console.log("RESSSS", res)
+  const local = moment.utc(createdTimestamp).local().format("YYYY-MM-DDTHH:mm:ss.SSSS")
+  const res = moment(local).fromNow()
+  console.log("RESW", res)
   return res
 }
 
